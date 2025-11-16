7ef3698ab60c371b6d0a93b3d640643b127448e4
Ali AKGÖZ <nukenssd@gmail.com>
2025-11-16 23:40:36 +0300

    chore: add Marker to react-simple-maps d.ts

--- src/types/react-simple-maps.d.ts
+++ src/types/react-simple-maps.d.ts
@@ -1,8 +1,18 @@
+// src/react-simple-maps.d.ts
 declare module "react-simple-maps" {
   import * as React from "react";
+
   export const ComposableMap: React.FC<any>;
   export const Geographies: React.FC<any>;
   export const Geography: React.FC<any>;
   export const ZoomableGroup: React.FC<any>;
+
+  // ---- Marker tipi ----
+  export interface MarkerProps {
+    coordinates: [number, number];
+    children?: React.ReactNode;
+    style?: any; // istersen sonradan sıkılaştırırız
+  }
+
   export const Marker: React.FC<MarkerProps>;
 }
